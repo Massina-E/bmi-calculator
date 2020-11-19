feeb776319a74886e28d9183622d442f805be58c
Massina <massina360@gmail.com>
2020-11-19 22:26:09 +0100

    First commit

---
+++ lib/constants.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kBottomContainerHeight = 80.0;
+const kColourCard = Color(0xFF1D1E33);
+const kInactiveColourCard = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 25.0, 
+  fontWeight: FontWeight.w900
+  );
+
+const kResultTitle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
